6d6b0101191ef82f0e336b9f8f01b608ed9c9c04
Indrajith C <indranspeaks@gmail.com>
2020-12-19 17:31:51 +0530

    basic setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright(c) 2020
+ * 
+ * All rights reserved.
+ * 
+ * @author Indrajith C
+ * 
+ */
+
+function App() {
+  return (
+    <div className="App">
+      hello there,
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright(c) 2020
+ * 
+ * All rights reserved.
+ * 
+ * @author Indrajith C
+ * 
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
