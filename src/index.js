395755892aa936703305e8660c22b6e73dd934be
Indrajith C <indranspeaks@gmail.com>
2020-12-19 17:40:33 +0530

    componentized files

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
+import reportWebVitals from './containers/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('c_amr')
 );
 
 reportWebVitals();
