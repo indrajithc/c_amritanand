d361ef23b6dd99ca34ac9903674d4bf53ab73f02
Indrajith C <indranspeaks@gmail.com>
2020-12-19 18:06:56 +0530

    style file added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 /*
  * Copyright(c) 2020
- * 
+ *
  * All rights reserved.
- * 
+ *
  * @author Indrajith C
- * 
+ *
  */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import reportWebVitals from './containers/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import reportWebVitals from "./containers/reportWebVitals";
+
+/** Bootstrap v5 */
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('c_amr')
+  document.getElementById("c_amr"),
 );
 
 reportWebVitals();
