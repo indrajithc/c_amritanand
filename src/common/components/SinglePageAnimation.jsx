ba218e4a78789f1faacc7c111f11052e091d03b0
Indrajith C <indranspeaks@gmail.com>
2020-12-25 17:45:20 +0530

    page in out amiation wrapper

---
+++ src/common/components/SinglePageAnimation.jsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+
+/**  ========= TAG COMPONENTS ========= */
+/**  ========= SUB COMPONENT ========= */
+/**  ========= MODULE STYLES ========= */
+/**  ========= CUSTOM COMPONENTS ========= */
+/**  ========= API SERVICE FUNCTIONS ========= */
+/**  ========= SVG IMAGE ICON ========= */
+/**  ========= UTILS ========= */
+
+const SinglePageAnimation = (props) => {
+  const { children, pageIn } = props;
+
+  const animationClass = useMemo(() => (pageIn ? "animate__zoomIn" : "animate__zoomOut"), [pageIn]);
+
+  return (
+    <div className={`animate__animated  ${animationClass}`}>
+      {children}
+    </div>
+  );
+};
+
+SinglePageAnimation.defaultProps = {
+  pageIn: false,
+};
+
+SinglePageAnimation.propTypes = {
+  children: PropTypes.objectOf(PropTypes.any).isRequired,
+  pageIn: PropTypes.bool,
+};
+
+export default SinglePageAnimation;
