50eabac7849038e3ee0696d2ff8bde2cd3b81e72
Indrajith C <indranspeaks@gmail.com>
2020-12-25 13:20:36 +0530

    page constants added

---
+++ src/common/utils/constants.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+
+const defaultConstants = {
+  TURE: true,
+};
+
+const pageConstants = {
+  HOME: "home",
+  ABOUT: "about",
+};
+
+export {
+  defaultConstants as default,
+  pageConstants,
+};
