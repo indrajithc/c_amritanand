ccd7b1cf37841019394378717589e759ca385ed4
Indrajith C <indranspeaks@gmail.com>
2020-12-26 21:04:14 +0530

    profile components export

---
+++ src/components/Profile/index.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+
+/** ========= TAG COMPONENTS ========= */
+/** ========= SUB COMPONENT ========= */
+/** ========= MODULE STYLES ========= */
+/** ========= CUSTOM COMPONENTS ========= */
+/** ========= API SERVICE FUNCTIONS ========= */
+/** ========= SVG IMAGE ICON ========= */
+/** ========= UTILS ========= */
+
+import JumbotronProfile from "./Jumbotron";
+
+const dummy = null;
+
+export {
+  JumbotronProfile as Jumbotron,
+  dummy,
+};
