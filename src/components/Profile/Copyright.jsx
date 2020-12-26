23370a37ef87e5ff3644c0d81caf86d0fde65c31
Indrajith C <indranspeaks@gmail.com>
2020-12-26 23:14:42 +0530

    copyright component

---
+++ src/components/Profile/Copyright.jsx
@@ -0,0 +1,33 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+
+import React from "react";
+
+/** ========= TAG COMPONENTS ========= */
+/** ========= SUB COMPONENT ========= */
+/** ========= MODULE STYLES ========= */
+/** ========= CUSTOM COMPONENTS ========= */
+/** ========= API SERVICE FUNCTIONS ========= */
+/** ========= SVG IMAGE ICON ========= */
+/** ========= UTILS ========= */
+
+const Copyright = () => (
+  <div className="copyright-wrapper">
+    <p className="content-text">
+      Made with
+      <span>❤️</span>
+      in India
+      | designed by
+      <a href="https://amritvin.github.io/c_amritanand/"> c.amritanand </a>
+      © 2018-2020
+    </p>
+  </div>
+);
+
+export default Copyright;
