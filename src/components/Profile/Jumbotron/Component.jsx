943eb6ae1abb3922ae8820e6559ed6e245545421
Indrajith C <indranspeaks@gmail.com>
2020-12-26 21:03:37 +0530

    profile jumbotron component

---
+++ src/components/Profile/Jumbotron/Component.jsx
@@ -0,0 +1,25 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+
+/** ========= TAG COMPONENTS ========= */
+/** ========= SUB COMPONENT ========= */
+/** ========= MODULE STYLES ========= */
+/** ========= CUSTOM COMPONENTS ========= */
+/** ========= API SERVICE FUNCTIONS ========= */
+/** ========= SVG IMAGE ICON ========= */
+/** ========= UTILS ========= */
+
+const JumbotronComponent = (props) => {
+  console.log("test", props);
+  console.log("test");
+
+  return (<div>div</div>);
+};
+
+export default JumbotronComponent;
---
+++ src/components/Profile/Jumbotron/Container.jsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+
+/** ========= TAG COMPONENTS ========= */
+/** ========= SUB COMPONENT ========= */
+import Component from "./Component";
+
+/** ========= MODULE STYLES ========= */
+/** ========= CUSTOM COMPONENTS ========= */
+/** ========= API SERVICE FUNCTIONS ========= */
+/** ========= SVG IMAGE ICON ========= */
+/** ========= UTILS ========= */
+
+const JumbotronContainer = (props) => {
+  console.log("test", props);
+
+  return (<Component />);
+};
+
+export default JumbotronContainer;
