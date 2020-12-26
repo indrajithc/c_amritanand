cb82ad91a95e6cee52a49e630092421244ac1d51
Indrajith C <indranspeaks@gmail.com>
2020-12-26 21:04:42 +0530

    profile jumbotron added

--- src/components/Profile/Jumbotron/Component.jsx
+++ src/components/Profile/Jumbotron/Component.jsx
@@ -17,9 +17,18 @@
 
 const JumbotronComponent = (props) => {
   console.log("test", props);
-  console.log("test");
 
-  return (<div>div</div>);
+  return (
+    <div className="jumbotron-profile">
+      <div className="w-100 row justify-content-center">
+        <div className="col-12 col-md-6 col-lg-5">
+          <div className="main-image ">
+            <img className="image-fluid mw-100" alt="" src="https://amritvin.github.io/c_amritanand/images/me12.jpeg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JumbotronComponent;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,24 @@
  */
 
 import React from "react";
+// import { Link } from "react-router-dom";
+// import { Parallax } from "react-scroll-parallax";
+import { Jumbotron as JumbotronProfile } from "../../components/Profile";
 
-const Home = () => (<div>Home</div>);
+/** ========= TAG COMPONENTS ========= */
+/** ========= SUB COMPONENT ========= */
+/** ========= MODULE STYLES ========= */
+/** ========= CUSTOM COMPONENTS ========= */
+/** ========= API SERVICE FUNCTIONS ========= */
+/** ========= SVG IMAGE ICON ========= */
+/** ========= UTILS ========= */
+
+const Home = () => (
+  <div className="horizontal-vertical-center">
+
+    <JumbotronProfile />
+
+  </div>
+);
 
 export default Home;
