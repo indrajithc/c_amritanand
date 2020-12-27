9bc20b71f545521000af0bf98f4cfb310de0e866
Indrajith C <indranspeaks@gmail.com>
2020-12-27 18:40:50 +0530

    scoll sizing anim added

--- src/components/Profile/Jumbotron/Component.jsx
+++ src/components/Profile/Jumbotron/Component.jsx
@@ -7,7 +7,7 @@
  *
  */
 
-import React from "react";
+import React, { useMemo } from "react";
 
 /** ========= TAG COMPONENTS ========= */
 /** ========= SUB COMPONENT ========= */
@@ -20,15 +20,38 @@ import Copyright from "../Copyright";
 /** ========= SVG IMAGE ICON ========= */
 /** ========= UTILS ========= */
 
+/**  ========= CONTEXT ========= */
+import { useSettings } from "../../../common/context/siteSettings";
+import { minMaxNumber, minMaxReducer } from "../../../common/utils/functionUtils";
+
 const JumbotronComponent = (props) => {
   console.log("test", props);
+
+  const [settings] = useSettings();
+
+  const scrollValues = useMemo(() => {
+    const { scroll, homePageCount } = settings;
+    const pageScrollFactor = (((scroll) || 0) / (100 / homePageCount));
+
+    const imageWith = 50 * pageScrollFactor;
+
+    return ({
+      imageWith: Math.max(0, Math.min(imageWith, 50)),
+      ratio: pageScrollFactor,
+    });
+  }, [settings]);
 
   return (
     <div className="jumbotron-profile">
       <div className="w-100 row justify-content-center">
         <div className="col-10 col-md-5 col-lg-4">
-          <div className="main-image w-50 mx-auto">
-            <img className="image-fluid mw-100" alt="" src="https://amritvin.github.io/c_amritanand/images/me12.jpeg" />
+          <div
+            className="main-image mx-auto transition-linear-01s"
+            style={{
+              width: `${50 + scrollValues.imageWith}%`,
+            }}
+          >
+            <img className="image-fluid w-100 mw-100" alt="" src="https://amritvin.github.io/c_amritanand/images/me12.jpeg" />
           </div>
         </div>
       </div>
@@ -37,7 +60,7 @@ const JumbotronComponent = (props) => {
         <div className="col-12 col-md-8 col-lg-6">
           <div className="main-name">
             <h1>Amritanand C</h1>
-            <div className="subtitle">
+            <div className="subtitle  transition-linear-01s" style={{ zoom: minMaxNumber((1.5 - scrollValues.ratio), 0.25, 1) }}>
               <h4>
                 Good, Better and Best.
               </h4>
@@ -53,7 +76,7 @@ const JumbotronComponent = (props) => {
 
       <div className="w-100 row justify-content-center">
         <div className="col-12 text-center d-flex ">
-          <div className="social-link-home-wrapper">
+          <div className="social-link-home-wrapper  transition-linear-01s" style={{ zoom: minMaxReducer((minMaxNumber((1.3 - scrollValues.ratio), 0, 1)), 0.5, 1) }}>
             <SocialLinks />
             <div className="underline" />
             <Copyright />
