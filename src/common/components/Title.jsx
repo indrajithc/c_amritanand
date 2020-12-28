ac5300ef8eff0cf9246d708640b00ba755a93009
Indrajith C <indranspeaks@gmail.com>
2020-12-28 23:06:04 +0530

    common title component

---
+++ src/common/components/Title.jsx
@@ -0,0 +1,53 @@
+/*
+ * Copyright(c) 2020
+ *
+ * All rights reserved.
+ *
+ * @author Indrajith C
+ *
+ */
+import React from "react";
+import PropTypes from "prop-types";
+
+/** ========= TAG COMPONENTS ========= */
+/** ========= SUB COMPONENT ========= */
+/** ========= MODULE STYLES ========= */
+/** ========= CUSTOM COMPONENTS ========= */
+/** ========= API SERVICE FUNCTIONS ========= */
+/** ========= SVG IMAGE ICON ========= */
+/** ========= UTILS ========= */
+
+const PageTitle = (props) => {
+  const {
+    children, className, underline, ...rest
+  } = props;
+
+  return (
+    <div className={` page-title  ${className}`} {...rest}>
+      <div className="content">
+        {children}
+      </div>
+      {
+        underline ? (<div className="title-underline" />) : ""
+      }
+    </div>
+  );
+};
+
+PageTitle.defaultProps = {
+  className: "",
+  underline: true,
+};
+
+PageTitle.propTypes = {
+  underline: PropTypes.bool,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+const dummy = null;
+
+export {
+  PageTitle,
+  dummy,
+};
